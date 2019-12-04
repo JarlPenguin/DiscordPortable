ce4296bcac7781df0e281a5a76fc1cb0620dfa67
Makazzz <makazzzpro@live.ca>
2019-12-03 19:15:42 -0500

    Remove binairies from repo
    EULA thing

--- App/Discord/app/app-0.0.301/resources/app/app_bootstrap/buildInfo.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var buildInfo = require('../build_info.json');
-
-exports.default = buildInfo;
-module.exports = exports['default'];
